fc2a2632a0afbcab2b366a1e03b09d632c8ca73f
mykola-dorofii-ntiloyalty <mykola.dorofii@ntiloyalty.com>
2023-02-10 11:49:11 +0200

    breading

--- contracts/breedings/Breeding.sol
+++ contracts/breedings/Breeding.sol
@@ -22,9 +22,11 @@ contract Breading is Ownable, IBreeding {
     using GenesUtil for uint256;
 
     uint256 private _globalSeed = 1;
-    uint256 private _randomPercent = 10;
-    uint256 private _minArcane = 5;
-    uint256 private _maxArcane = 10;
+    uint256 private _randomPercent = 5;
+    uint256 private _minArcane = 1;
+    uint256 private _maxArcane = 30;
+    uint256 private _minId = 1;
+    uint256 private _maxId = 36;
 
     function breading(uint256 genes1, uint256 genes2)
         external
@@ -47,42 +49,47 @@ contract Breading is Ownable, IBreeding {
 
         _globalSeed++;
 
-        uint32 generation = (GenesUtil.getGeneration(genes1) + GenesUtil.getGeneration(genes2)) / 2 + 1;
+        uint32 generation = (GenesUtil.getGeneration(genes1) +
+            GenesUtil.getGeneration(genes2)) /
+            2 +
+            1;
 
-        uint8 minId = 1;
-        uint8 maxId = 36;
         uint8 bodyPartsCount = 9;
         uint256 randomIndex = 0;
 
         uint256 genes = 0;
 
         // body parts
-        for (uint8 i = 0; i < bodyPartsCount; i++) {
+        for (
+            uint8 bodyPartIndex = 0;
+            bodyPartIndex < bodyPartsCount;
+            bodyPartIndex++
+        ) {
             uint256 arcanePercent = getArcanePercent(
                 generation,
                 _minArcane,
                 _maxArcane,
                 100
             );
-            bool isArc = arcanePercent <
+            bool isArc = arcanePercent >=
                 random(_globalSeed, randomIndex++, 0, _maxArcane * 100);
 
             uint8 id = 1;
-            if (_randomPercent < random(_globalSeed, randomIndex++, 0, 100)) {
+            if (_randomPercent >= random(_globalSeed, randomIndex++, 0, 100)) {
                 id = uint8(
-                    random(_globalSeed, randomIndex++, minId, maxId + 1) & 0xFF
+                    random(_globalSeed, randomIndex++, _minId, _maxId + 1) & 0xFF
                 );
             } else {
                 if (random(_globalSeed, randomIndex++, 0, 100) >= 50) {
-                    id = GenesUtil.getId(genes2, i);
+                    id = GenesUtil.getId(genes2, bodyPartIndex);
                 } else {
-                    id = GenesUtil.getId(genes1, i);
+                    id = GenesUtil.getId(genes1, bodyPartIndex);
                 }
             }
 
-            genes = GenesUtil.setId(genes, i, id);
-            genes = GenesUtil.setLevel(genes, i, 1);
-            genes = GenesUtil.setArcane(genes, i, isArc ? 1 : 0);
+            genes = GenesUtil.setId(genes, bodyPartIndex, id);
+            genes = GenesUtil.setLevel(genes, bodyPartIndex, 1);
+            genes = GenesUtil.setArcane(genes, bodyPartIndex, isArc ? 1 : 0);
         }
 
         // set generation
@@ -137,6 +144,26 @@ contract Breading is Ownable, IBreeding {
     }
 
     // --------------------------------------------------------------------
+
+    function getMinId() external view returns (uint256) {
+        return _minId;
+    }
+
+    function setMinId(uint256 value) public onlyOwner {
+        _minId = value;
+    }
+
+    // --------------------------------------------------------------------
+
+    function getMaxId() external view returns (uint256) {
+        return _maxId;
+    }
+
+    function setMaxId(uint256 value) public onlyOwner {
+        _maxId = value;
+    }
+
+    // --------------------------------------------------------------------
     // RANDOM
     // --------------------------------------------------------------------
 
@@ -166,6 +193,6 @@ contract Breading is Ownable, IBreeding {
         if (generation <= min) {
             return 0;
         }
-        return (1 * (generation * mult - min * mult)) / (max);
+        return ((generation - min) * mult) / max;
     }
 }
